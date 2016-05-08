2a039281e24c04dbd04408c931e3d99686476f99
Marcin Mejran <marcin.mejran@komodohealth.com>
2016-05-08 12:43:46 -0700

    Add support for disabling recursive versioning

--- src/main/scala/sativum/DatedTask.scala
+++ src/main/scala/sativum/DatedTask.scala
@@ -10,6 +10,13 @@ trait DatedTask extends Task[Any] {
   val partition: LocalDate
   override lazy val name: String = baseName+ "/" + partition.toString()
   override lazy val versionName = baseName
-  override lazy val dir = p.path + "/" + baseName + "/" + recursiveVersionShort + "/" +
-    partition.toString("yyyy/MM/dd") + "/"
+  override lazy val dir =
+    if(p.recursiveVersioning) {
+      p.path + "/" + baseName + "/" + recursiveVersionShort + "/" +
+        partition.toString("yyyy/MM/dd") + "/"
+    } else {
+      p.path + "/" + baseName + "/" + "latest" + "/" +
+        partition.toString("yyyy/MM/dd") + "/"
+    }
+
 }
