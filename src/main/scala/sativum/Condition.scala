2fdced8bfd1b32127cea82f3b67aa81b1e9ea658
Marcin Mejran <marcin.mejran@komodohealth.com>
2016-05-12 11:00:04 -0400

    Support for conditional tasks which only are valid to run for certain date ranges but not others

---
+++ src/main/scala/sativum/Condition.scala
@@ -0,0 +1,8 @@
+package sativum
+
+/**
+  * Created by marcin.mejran on 5/9/16.
+  */
+trait Condition {
+  def condition(): Boolean
+}
---
+++ src/main/scala/sativum/Conditional.scala
@@ -0,0 +1,8 @@
+package sativum
+
+/**
+  * Created by marcin.mejran on 5/9/16.
+  */
+trait Conditional {
+
+}
